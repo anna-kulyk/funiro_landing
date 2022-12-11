794b9a2814ab6283a07987e6205f376781838a89
Anna <kulyk_anna@outlook.com>
2022-12-10 23:51:41 -0500

    added js for nav on mobile devices 768+

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// const { doc } = require("prettier");
+
 let menu = document.querySelector(".menu__body");
 let menuIcon = document.querySelector(".icon-menu");
 
@@ -27,3 +29,23 @@ function ibg(){
 }
 
 ibg();
+
+//==========================================================================
+
+let isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+
+window.onload = function () {
+    document.addEventListener('click', documentActions);
+
+    function documentActions(e) {
+        const targetElement = e.target;
+        if (window.innerWidth > 768 && isMobile.any()) {
+            if (targetElement.classList.contains('menu__arrow')) {
+                targetElement.closest('.menu__item').classList.toggle('_hover');
+            }
+            if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0) {
+                document.querySelectorAll('.menu__item._hover').forEach(el => el.classList.remove('_hover'));
+            }
+        }
+    }
+}
