098f5cbb0fc94a32db446b2056ab3fc3c98d7238
Anna <kulyk_anna@outlook.com>
2023-01-19 11:38:32 -0500

    started slider section, added swiper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import '@/styles/index.scss';
 
 import '@/js/script.js';
+import '@/js/slider.js';
 
 // import avatar from './assets/images/avatar.jpg';
 // document.querySelector('#avatar').src = avatar;
